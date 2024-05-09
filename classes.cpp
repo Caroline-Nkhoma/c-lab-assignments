dc851fd6807892306eea65caa9e6ca40af6c7d67
Caroline-Nkhoma <bsc-com-35-22@unima.ac.mw>
2024-05-09 03:53:42 +0200

    added lab task 7

---
+++ classes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+class rectangle
+{
+    private:
+    
+};  
+
+int main(){  
+    return 0;
+}
---
+++ pointers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+void swapNumbers(int &a, int &b);
+using namespace std;
+int main()
+{
+
+    int varA = 25;
+    int varB = 100;
+
+    cout << "varA before swap:" << varA << endl; // varA is 25
+    cout << "varB before swap:" << varB << endl; // varB is 100
+
+    swapNumbers(varA, varB);
+
+    cout << "varA after swap:" << varA << endl;  // varA is 100
+    cout << "varB before swap:" << varB << endl; // varB is 25
+
+    return 0;
+}
+void swapNumbers(int &a, int &b)
+{
+    int refers = a;
+    a = b;
+    b = refers;
+}
